71887ddb4c7415859ed91d46101190ba8bddc934
Bertware <contact@bertware.net>
2017-06-21 14:22:16 +0200

    Show train name + destination as subtitle #7

--- app/src/main/java/android/irail/be/hyperrail/TrainActivity.java
+++ app/src/main/java/android/irail/be/hyperrail/TrainActivity.java
@@ -157,7 +157,7 @@ public class TrainActivity extends RecyclerViewActivity<Train> implements onRecy
     }
 
     protected void showData(Train train) {
-        setSubTitle(train.getName());
+        setSubTitle(train.getName() + " " + train.getName());
         if (getSupportActionBar() != null) {
             getSupportActionBar().setSubtitle(train.getDirection().getLocalizedName());
         }
