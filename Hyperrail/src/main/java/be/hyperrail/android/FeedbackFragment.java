3111b1a885e23cc8d8209bb269e3cb04856e663f
Bertware <contact@bertware.net>
2017-06-25 11:12:59 +0200

    Feedback and github link, fixed #28

---
+++ Hyperrail/src/main/java/be/hyperrail/android/FeedbackFragment.java
@@ -0,0 +1,65 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package be.hyperrail.android;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FeedbackFragment extends Fragment {
+
+    public FeedbackFragment() {
+        // Required empty public constructor
+    }
+
+    public static FeedbackFragment newInstance() {
+        return new FeedbackFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feedback, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = FeedbackFragment.this.getActivity();
+                String version = "unknown";
+
+                try {
+                    PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+                    version = pInfo.versionName;
+                } catch (Exception e) {
+                    // Ignored
+                }
+
+                Intent feedbackEmail = new Intent(Intent.ACTION_SEND);
+
+                feedbackEmail.setType("text/email");
+                feedbackEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"feedback@hyperrail.be"});
+                feedbackEmail.putExtra(Intent.EXTRA_SUBJECT, "Feedback for hyperrail " + version);
+                startActivity(Intent.createChooser(feedbackEmail, "Send feedback mail:"));
+            }
+        });
+    }
+}
