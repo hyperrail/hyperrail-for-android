b7d7932336cd4450b8684cdc2b1bd5b2d4742e80
Bertware <contact@bertware.net>
2017-11-12 18:00:13 +0100

    Always show an explanation before requesting the user location

--- Hyperrail/src/main/java/be/hyperrail/android/LiveboardSearchFragment.java
+++ Hyperrail/src/main/java/be/hyperrail/android/LiveboardSearchFragment.java
@@ -13,7 +13,9 @@
 package be.hyperrail.android;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -24,7 +26,6 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -323,31 +324,33 @@ public class LiveboardSearchFragment extends Fragment implements OnRecyclerItemC
                 != PackageManager.PERMISSION_GRANTED) {
 
             // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            // Explain anywau
+            /*if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) */
 
-                Intent i = new Intent(getActivity(), PermissionRequestExplanation.class);
-                i.putExtra("title", getResources().getString(R.string.permission_description_location_title));
-                i.putExtra("description", getResources().getString(R.string.permission_description_location_description));
-                i.putExtra("icon", R.drawable.ic_location_on_48);
-                i.putExtra("permission", Manifest.permission.ACCESS_COARSE_LOCATION);
-                i.putExtra("preference", "stations_enable_nearby");
-                startActivity(i);
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(
-                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                            COARSE_LOCATION_REQUEST);
+            AlertDialog explanation = (new AlertDialog.Builder(this.getActivity())).create();
+            explanation.setTitle(R.string.permission_description_location_title);
+            explanation.setMessage(getResources().getString(R.string.permission_description_location_description));
+            explanation.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.Yes), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Ask
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                       LiveboardSearchFragment.this.requestPermissions(
+                                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                COARSE_LOCATION_REQUEST);
+                    }
                 }
+            });
+            explanation.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.No), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Disable
+                    PreferenceManager.getDefaultSharedPreferences(LiveboardSearchFragment.this.getActivity()).edit().putBoolean("stations_enable_nearby", false).apply();
+                }
+            });
 
-            }
+            explanation.show();
         } else {
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                     mGoogleApiClient);
