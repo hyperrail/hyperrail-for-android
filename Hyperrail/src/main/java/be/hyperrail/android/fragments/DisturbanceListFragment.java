058f31205ef570a43b359d2bbb5b0a5494db61b3
Bertware <bert.marcelis@outlook.com>
2018-02-06 11:17:16 +0100

    Moved disturbances results to a fragment #75

--- Hyperrail/src/main/java/be/hyperrail/android/fragments/DisturbanceListFragment.java
+++ Hyperrail/src/main/java/be/hyperrail/android/fragments/DisturbanceListFragment.java
@@ -4,30 +4,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
 
 package be.hyperrail.android.fragments;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,17 +32,17 @@ import be.hyperrail.android.util.ErrorDialogFactory;
 /**
  * A list with disturbances
  */
-public class DisturbanceListFragment extends Fragment implements OnRecyclerItemClickListener<Disturbance> {
+public class DisturbanceListFragment extends RecyclerViewFragment<Disturbance[]> implements ResultFragment<IrailDisturbanceRequest>, OnRecyclerItemClickListener<Disturbance> {
 
-    private RecyclerView vRecyclerView;
-    private SwipeRefreshLayout vRefreshLayout;
     private Disturbance[] disturbances;
     private DateTime lastUpdate;
 
     DisturbanceCardAdapter disturbanceCardAdapter;
+    private IrailDisturbanceRequest mRequest;
 
     public DisturbanceListFragment() {
         // Required empty public constructor
+        mRequest = new IrailDisturbanceRequest();
     }
 
     public static DisturbanceListFragment newInstance() {
@@ -69,47 +53,56 @@ public class DisturbanceListFragment extends Fragment implements OnRecyclerItemC
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_disturbance_list, container, false);
+        return inflater.inflate(R.layout.fragment_recyclerview_list, container, false);
     }
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
 
-        vRefreshLayout = view.findViewById(R.id.swiperefresh);
-        vRefreshLayout.setOnRefreshListener(
-                new SwipeRefreshLayout.OnRefreshListener() {
-                    @Override
-                    public void onRefresh() {
-                        // This method performs the actual data-refresh operation.
-                        // The method calls setRefreshing(false) when it's finished.
-                        loadDisturbances();
-                    }
-                }
-        );
+    @Override
+    protected RecyclerView.Adapter getAdapter() {
+        if (disturbanceCardAdapter == null) {
+            disturbanceCardAdapter = new DisturbanceCardAdapter(getActivity(), null);
+        }
+        return disturbanceCardAdapter;
+    }
 
-        vRecyclerView = view.findViewById(R.id.recyclerview_primary);
+    @Override
+    protected void getData() {
+        vRefreshLayout.setRefreshing(true);
 
-        vRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
-        vRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        IrailFactory.getDataProviderInstance().abortAllQueries();
 
-        if (!PreferenceManager.getDefaultSharedPreferences(getActivity().getApplication()).getBoolean("use_card_layout", false)) {
-            // Cards have their own division by margin, others need a divider
-            vRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        IrailDisturbanceRequest request = new IrailDisturbanceRequest();
+        request.setCallback(new IRailSuccessResponseListener<Disturbance[]>() {
+            @Override
+            public void onSuccessResponse(@NonNull Disturbance[] data, Object tag) {
+                vRefreshLayout.setRefreshing(false);
+                lastUpdate = new DateTime();
+                showData(data);
+            }
+        }, new IRailErrorResponseListener() {
+            @Override
+            public void onErrorResponse(@NonNull Exception e, Object tag) {
+                vRefreshLayout.setRefreshing(false);
+                // Don't finish, this is the main activity
+                ErrorDialogFactory.showErrorDialog(e, DisturbanceListFragment.this.getActivity(), false);
+            }
+        }, null);
+        IrailFactory.getDataProviderInstance().getDisturbances(request);
+    }
+
+    @Override
+    protected void showData(Disturbance[] data) {
+        if (data == null) {
+            return;
         }
 
-        disturbanceCardAdapter = new DisturbanceCardAdapter(this.getActivity().getApplicationContext(), null);
-        disturbanceCardAdapter.setOnItemClickListener(this);
-        vRecyclerView.setAdapter(disturbanceCardAdapter);
-
-        if (savedInstanceState != null && savedInstanceState.containsKey("updated")) {
-            this.disturbances = (Disturbance[]) savedInstanceState.getSerializable("disturbances");
-            this.lastUpdate = new DateTime(savedInstanceState.getLong("updated"));
-            this.setData(this.disturbances);
-        } else {
-            loadDisturbances();
-        }
+        this.disturbances = data;
+        disturbanceCardAdapter.updateDisturbances(disturbances);
     }
 
     @Override
@@ -121,44 +114,36 @@ public class DisturbanceListFragment extends Fragment implements OnRecyclerItemC
         }
     }
 
-    private void loadDisturbances() {
-        vRefreshLayout.setRefreshing(true);
-
-        IrailFactory.getDataProviderInstance().abortAllQueries();
-
-        IrailDisturbanceRequest request = new IrailDisturbanceRequest();
-        request.setCallback(new IRailSuccessResponseListener<Disturbance[]>() {
-            @Override
-            public void onSuccessResponse(Disturbance[] data, Object tag) {
-                vRefreshLayout.setRefreshing(false);
-                lastUpdate = new DateTime();
-                setData(data);
-            }
-        }, new IRailErrorResponseListener() {
-            @Override
-            public void onErrorResponse(Exception e, Object tag) {
-                vRefreshLayout.setRefreshing(false);
-                // Don't finish, this is the main activity
-                ErrorDialogFactory.showErrorDialog(e, DisturbanceListFragment.this.getActivity(), false);
-            }
-        }, null);
-        IrailFactory.getDataProviderInstance().getDisturbances(request);
-    }
-
-    private void setData(Disturbance[] disturbances) {
-        if (disturbances == null) {
-            return;
-        }
-
-        this.disturbances = disturbances;
-        disturbanceCardAdapter.updateDisturbances(disturbances);
-    }
-
     @Override
     public void onRecyclerItemClick(RecyclerView.Adapter sender, Disturbance disturbance) {
         if (disturbance.getLink() != null && !disturbance.getLink().isEmpty()) {
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(disturbance.getLink()));
             startActivity(browserIntent);
         }
+    }
+
+    @Override
+    public void setRequest(@NonNull IrailDisturbanceRequest request) {
+        this.mRequest = request;
+    }
+
+    @Override
+    public IrailDisturbanceRequest getRequest() {
+        return mRequest;
+    }
+
+    @Override
+    public void loadNextRecyclerviewItems() {
+        // Nothing to do - not supported
+    }
+
+    @Override
+    public void loadPreviousRecyclerviewItems() {
+        // Nothing to do - not supported
+    }
+
+    @Override
+    public void onDateTimePicked(DateTime d) {
+        // Nothing to do - not supported
     }
 }
